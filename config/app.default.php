93a2b5ed3a18dd9462999955145f623b08a4c63f
sra <z-akmoto@sra.co.jp>
2018-03-06 12:35:00 +0900

    edit

--- config/app.default.php
+++ config/app.default.php
@@ -231,7 +231,7 @@ return [
             //'port' => 'non_standard_port_number',
             'username' => 'root',
             'password' => 'WJb5gbco6',
-            'database' => 'medaka',
+            'database' => 'medaka01',
             'encoding' => 'utf8',
             'timezone' => 'UTC',
             'flags' => [],
